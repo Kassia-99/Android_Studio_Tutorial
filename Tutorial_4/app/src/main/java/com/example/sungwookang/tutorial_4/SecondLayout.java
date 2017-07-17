d745a55b24c4664727c7a8356bda93247aed604d
sungwoo99 <gkakwprl@naver.com>
2017-07-17 19:43:46 +0900

    make Tutorial_4

---
+++ Tutorial_4/app/src/main/java/com/example/sungwookang/tutorial_4/SecondLayout.java
@@ -0,0 +1,22 @@
+package com.example.sungwookang.tutorial_4;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by SungWoo Kang on 2017-07-17.
+ */
+
+public class SecondLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.second_layout, container, false);
+        return v;
+    }
+}
