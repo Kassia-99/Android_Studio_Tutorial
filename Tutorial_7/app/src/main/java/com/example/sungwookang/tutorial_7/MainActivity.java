9324bec61346c553e2ccbd4395cd9d0dab75a47a
sungwoo99 <gkakwprl@naver.com>
2017-07-19 20:59:37 +0900

    make Tutorial_7

---
+++ Tutorial_7/app/src/main/java/com/example/sungwookang/tutorial_7/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.sungwookang.tutorial_7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    YouTubePlayerView youTubeView;
+    Button button;
+    YouTubePlayer.OnInitializedListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.youtubeButton);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
+        listener = new YouTubePlayer.OnInitializedListener() {
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("nCkpzqqog4k");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                youTubeView.initialize("AIzaSyAbHV-ZymEblHH9MprJCqtERgiyZrlxFVQ", listener);
+            }
+        });
+    }
+}
