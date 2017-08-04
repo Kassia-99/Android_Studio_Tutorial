08ef5880ef88ce8e08d00da63579ab1885bc2ad4
Kassia-99 <gkakwprl@gmail.com>
2017-08-04 13:53:05 +0900

    make User Management Project: Step 2

--- Management/app/src/main/java/com/example/sungwookang/management/MainActivity.java
+++ Management/app/src/main/java/com/example/sungwookang/management/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sungwookang.management;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
     }
 }
