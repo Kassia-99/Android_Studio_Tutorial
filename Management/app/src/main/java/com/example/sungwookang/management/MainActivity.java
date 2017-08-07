0ea843d553de12b70a3785c9b78f4d4820263489
Kassia-99 <gkakwprl@gmail.com>
2017-08-07 14:52:07 +0900

    make User Management Project: Step 6

--- Management/app/src/main/java/com/example/sungwookang/management/MainActivity.java
+++ Management/app/src/main/java/com/example/sungwookang/management/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.sungwookang.management;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         TextView idText = (TextView) findViewById(R.id.idText);
         TextView passwordText = (TextView) findViewById(R.id.passwordText);
         TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
+        Button managementButton = (Button) findViewById(R.id.managementButton);
 
         Intent intent = getIntent();
         String userID = intent.getStringExtra("userID");
@@ -27,5 +37,61 @@ public class MainActivity extends AppCompatActivity {
         idText.setText(userID);
         passwordText.setText(userPassword);
         welcomeMessage.setText(message);
+
+        if (!userID.equals("admin")){
+            //managementButton.setEnabled(false);             // 버튼이 눌리지 않음
+            managementButton.setVisibility(View.GONE);      // 버튼이 보이지 않음
+        }
+
+        managementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new BackgroundTask().execute();
+            }
+        });
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String> {
+        String target;
+
+        @Override
+        protected void onPreExecute() {
+            target = "http://lazytitan.dothome.co.kr/Management/List.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            Intent intent = new Intent(MainActivity.this, ManagementActivity.class);
+            intent.putExtra("userList", result);
+            MainActivity.this.startActivity(intent);
+        }
     }
 }
