e29c10568d40e93803ba9eb4ed783cfec14f63cd
Kassia-99 <gkakwprl@gmail.com>
2017-08-07 11:48:37 +0900

    make User Management Project: Step 5

---
+++ Management/app/src/main/java/com/example/sungwookang/management/LoginRequest.java
@@ -0,0 +1,29 @@
+package com.example.sungwookang.management;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by SungWoo Kang on 2017-08-06.
+ */
+
+public class LoginRequest extends StringRequest {
+
+    final static private String URL = "http://lazytitan.dothome.co.kr/Management/Login.php";
+    private Map<String, String> parameters;
+
+    public LoginRequest(String userID, String userPassword, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
