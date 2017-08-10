bafad2a22ad60cc7abc19047567db6a8f128c381
Kassia-99 <gkakwprl@gmail.com>
2017-08-10 10:58:24 +0900

    make User Management Project: Step 9

--- Management/app/src/main/java/com/example/sungwookang/management/ManagementActivity.java
+++ Management/app/src/main/java/com/example/sungwookang/management/ManagementActivity.java
@@ -3,6 +3,9 @@ package com.example.sungwookang.management;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,6 +20,7 @@ public class ManagementActivity extends AppCompatActivity {
     private ListView listView;
     private UserListAdapter adapter;
     private List<User> userList;
+    private List<User> saveList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,8 @@ public class ManagementActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView);
         userList = new ArrayList<User>();
-        adapter = new UserListAdapter(getApplicationContext(), userList, this);
+        saveList = new ArrayList<User>();
+        adapter = new UserListAdapter(getApplicationContext(), userList, this, saveList);
         listView.setAdapter(adapter);
 
         try {
@@ -41,13 +46,42 @@ public class ManagementActivity extends AppCompatActivity {
                 userName = object.getString("userName");
                 userAge = object.getString("userAge");
                 User user = new User(userID, userPassword, userName, userAge);
-                if(!userID.equals("admin"))
+                if(!userID.equals("admin")) {
                     userList.add(user);
+                    saveList.add(user);
+                }
                 count++;
             }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+
+        final EditText search = (EditText) findViewById(R.id.search);
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void searchUser(String search) {
+        userList.clear();
+        for (int i = 0; i < saveList.size(); i++) {
+            if (saveList.get(i).getUserID().contains(search))
+                userList.add(saveList.get(i));
+        }
+        adapter.notifyDataSetChanged();
     }
 }
